9316ed8c99a7b78e691757bbd14676b982e2966e
chuanfeng <i@liuchuanfeng.cn>
2019-04-26 16:52:57 +0800

    添加prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    printWidth: 160,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    useTabs: false,
+};
