b7c2b54c8598b276c51de817805272583967bee3
chuanfeng <i@liuchuanfeng.cn>
2019-04-26 16:51:33 +0800

    优化php-formatter代码

--- vscode-php-formatter/php.js
+++ vscode-php-formatter/php.js
@@ -1,10 +1,38 @@
-'use strict';
 const output = require('./output');
 let leval = 0;
 let string = false;
 
-function formatCode(code) {
-    if (code.substring(0, 5) !== '<?php') {
+const setSpace = code => code
+    .replace(/ ?([\+\-\*\/\.\?!><]?={1,3})(?!\>) ?/g, ' $1 ') // `=` [>1]
+    .replace(/ ?([\&\|]{2}) ?/g, ' $1 ') // `$$` `||` [>1]
+    .replace(/ *(,) ?(?!\n)/g, '$1 ') // `,` [0, >1]
+    .replace(/\n* *(\{)/g, ' $1') // before `{` [1]
+    .replace(/ *(\))/g, ' $1') // before `)` [1]
+    .replace(/(\() */g, '$1 ') // after `(` [1]
+    .replace(/\(\s*\)/g, '()') // blank `()` `[]`
+    .replace(/(if|for|each)\s*\(/g, '$1 (') // after `if` `for` and `each`
+    .replace(/\}\s*else\s*\{/g, '} else {'); // blank `else`
+
+const formatSnippets = (code, index) => (index % 2 ? code : setSpace(code));
+
+const indentSnippets = code => {
+    if (!string) {
+        // trim
+        code = code.replace(/(^\s*)|(\s*$)/g, '');
+        // indent
+        if ('})]'.includes(code.charAt(0))) leval--;
+        if (code) code = `${' '.repeat(leval * 4)}${code}`;
+        if ('{(['.includes(code.charAt(code.length - 1))) leval++;
+    }
+    const quotes = (code.match(/"/g) || []).length;
+    if (quotes > 0 && quotes % 2 === 1) string = !string;
+    return code;
+};
+
+const separatCode = (code, separator, callback, joiner) => code.split(separator).map(callback).join(joiner || separator);
+
+const format = code => {
+    if (!code.startsWith('\<?php')) {
         output('[formatCode] Skip');
         return code;
     }
@@ -21,52 +49,6 @@ function formatCode(code) {
     code = separatCode(code, '"', formatSnippets);
     code = code.replace(/slantandquote/g, '\\"');
     return code;
-}
+};
 
-function separatCode(code, separator, callback, joiner) {
-    const codes = code.split(separator);
-    for (const i in codes) {
-        codes[i] = callback(codes[i], i);
-    }
-    return codes.join(joiner || separator);
-}
-
-function indentSnippets(code) {
-    if (!string) {
-        // trim
-        code = code.replace(/(^\s*)|(\s*$)/g, '');
-        // indent
-        if ('})]'.indexOf(code.charAt(0)) >= 0) leval--;
-        if (code) {
-            for (let i = 0; i < leval * 4; i++) {
-                code = ` ${code}`;
-            }
-        }
-        if ('{(['.indexOf(code.charAt(code.length - 1)) >= 0) leval++;
-    }
-    const quotes = (code.match(/"/g) || []).length;
-    if (quotes > 0 && quotes % 2 === 1) string = !string;
-    return code;
-}
-
-function formatSnippets(code, index) {
-    if (index % 2 === 0) {
-        code = setSpace(code);
-    }
-    return code;
-}
-
-function setSpace(code) {
-    return code
-        .replace(/ ?([\+\-\*\/\.\?!><]?={1,3})(?!\>) ?/g, ' $1 ') // `=` [>1]
-        .replace(/ ?([\&\|]{2}) ?/g, ' $1 ') // `$$` `||` [>1]
-        .replace(/ *(,) ?(?!\n)/g, '$1 ') // `,` [0, >1]
-        .replace(/\n* *(\{)/g, ' $1') // before `{` [1]
-        .replace(/ *(\))/g, ' $1') // before `)` [1]
-        .replace(/(\() */g, '$1 ') // after `(` [1]
-        .replace(/\(\s*\)/g, '()') // blank `()` `[]`
-        .replace(/(if|for|each)\s*\(/g, '$1 (') // after `if` `for` and `each`
-        .replace(/\}\s*else\s*\{/g, '} else {'); // blank `else`
-}
-
-module.exports = formatCode;
+module.exports = format;
