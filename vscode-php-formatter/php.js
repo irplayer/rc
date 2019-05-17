f25ceeb6102568fd38a21e184510192079c59072
chuanfeng <i@liuchuanfeng.cn>
2019-05-17 14:19:13 +0800

    重构php代码格式化脚本

--- vscode-php-formatter/php.js
+++ vscode-php-formatter/php.js
@@ -1,54 +1,43 @@
 const output = require('./output');
-let leval = 0;
-let string = false;
 
-const setSpace = code => code
-    .replace(/ ?([\+\-\*\/\.\?!><]?={1,3})(?!\>) ?/g, ' $1 ') // `=` [>1]
-    .replace(/ ?([\&\|]{2}) ?/g, ' $1 ') // `$$` `||` [>1]
-    .replace(/ *(,) ?(?!\n)/g, '$1 ') // `,` [0, >1]
-    .replace(/\n* *(\{)/g, ' $1') // before `{` [1]
-    .replace(/ *(\))/g, ' $1') // before `)` [1]
-    .replace(/(\() */g, '$1 ') // after `(` [1]
-    .replace(/\(\s*\)/g, '()') // blank `()` `[]`
-    .replace(/(if|for|each)\s*\(/g, '$1 (') // after `if` `for` and `each`
-    .replace(/\}\s*else\s*\{/g, '} else {'); // blank `else`
-
-const formatSnippets = (code, index) => (index % 2 ? code : setSpace(code));
-
-const indentSnippets = code => {
-    if (!string) {
-        // trim
-        code = code.replace(/(^\s*)|(\s*$)/g, '');
-        // indent
+const format = code => {
+    let leval = 0;
+    const indentSnippets = code => {
+        code = code.trim();
         if ('})]'.includes(code.charAt(0))) leval--;
         if (code) code = `${' '.repeat(leval * 4)}${code}`;
         if ('{(['.includes(code.charAt(code.length - 1))) leval++;
-    }
-    const quotes = (code.match(/"/g) || []).length;
-    if (quotes > 0 && quotes % 2 === 1) string = !string;
-    return code;
-};
-
-const separatCode = (code, separator, callback, joiner) => code.split(separator).map(callback).join(joiner || separator);
-
-const format = code => {
-    if (!code.startsWith('\<?php')) {
-        output('[formatCode] Skip');
         return code;
-    }
+    };
     output('[formatCode] Start');
-    leval = 0;
-    string = false;
-    // 设置缩进并去除尾随空格
-    code = separatCode(code, /\r?\n/, indentSnippets, '\n');
-    // 函数/类前添加空行
-    code = code.replace(/([^\{])\n+( *(public |private )?(function |class ))/g, '$1\n\n$2');
-    // 仅保留一个空行
-    code = code.replace(/(\n{2,})/g, '\n\n');
-    code = code.replace(/\\"/g, 'slantandquote');
-    code = separatCode(code, '"', formatSnippets);
-    code = code.replace(/slantandquote/g, '\\"');
-    return code;
+    const contents = [];
+    return code
+        .replace(/(['"])([\s\S]*?)(\1)/g, (_exp, q, content) => ((contents.push(content), `${q}quotestring${q}`)))
+
+        .replace(/ ?([\+\-\*\/\.\?!><]?={1,3})(?!\>) ?/g, ' $1 ') // `=` [>1]
+        .replace(/ ?([\&\|]{2}) ?/g, ' $1 ') // `&&` `||` [>1]
+        .replace(/ *(,) ?(?!\n)/g, '$1 ') // `,` [0, >1]
+        .replace(/\n* *(\{)/g, ' $1') // before `{` [1]
+
+        .replace(/(\() */g, '$1 ') // after  `(` [1]
+        .replace(/ *(\))/g, ' $1') // before `)` [1]
+        .replace(/\(\s*\)/g, '()') // `()`
+
+        .replace(/(if|for|each)\s*\(/g, '$1 (') // after `if` `for` and `each`
+
+        .replace(/([\{\}])(.)/g, '$1\n$2') // after `{` `}` [LF]
+        .replace(/\}\s*(else|catch)/g, '} $1') // after  `else` except
+        .replace(/(else|catch)\s*\{/g, '$1 {') // before `else` except
+
+        .replace(/[；;](.)/g, ';\n$1') // after `;` [LF]
+        .replace(/for \([\s\S]*?\)/g, exp => exp.replace(/;\s+/g, '; ')) // `for` except
+
+        .split(/\r?\n/).map(indentSnippets).join('\n') // 设置缩进
+
+        .replace(/([^\{])\n+( *(public |private )?(function |class ))/g, '$1\n\n$2') // 函数/类前添加空行
+        .replace(/(\n{2,})/g, '\n\n') // 去除多余空行
+
+        .replace(/(['"]).*?(\1)/g, (_exp, q, content) => (((content = contents.shift()), q === '"' && content.match(/[\$\n']/g) ? `"${content}"` : `'${content}'`)));
 };
 
-module.exports = format;
+module.exports = code => code.startsWith('\<?php') ? format(code) : code;
